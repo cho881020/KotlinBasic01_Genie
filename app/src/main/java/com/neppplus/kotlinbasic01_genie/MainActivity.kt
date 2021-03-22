642357ebed8d08284aae82360f0c7524e7ffec1a
Geine <cho881020@gmail.com>
2021-03-22 12:57:58 +0900

    로그 찍어보기

--- app/src/main/java/com/neppplus/kotlinbasic01_genie/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic01_genie/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.kotlinbasic01_genie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        clickBtn.setOnClickListener {
+
+            // 코드에 메모 남기기 - 주석
+//            Ctrl + / : 커서가 있는 줄을 주석 처리.
+//            이 {  } 안 에 있는 코드만, 버튼이 눌렸을때 실행됨.
+
+            Log.d("메인화면로그", "클릭용 버튼 눌림")
+
+        }
 
     }
 
